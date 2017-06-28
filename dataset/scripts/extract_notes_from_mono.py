3a4bf3f5a2da1c81cfd53c52a9029190e853fa95
mr.Shu <mr@shu.io>
2017-06-28 10:00:44 +0200

    deletels
    Signed-off-by: mr.Shu <mr@shu.io>

--- dataset/scripts/extract_notes_from_mono.py
+++ dataset/scripts/extract_notes_from_mono.py
@@ -2,6 +2,8 @@ from mido import Message, MidiFile, MidiTrack
 import sys
 
 mid = MidiFile(sys.argv[1])
+notes = []
 for msg in mid.tracks[0]:
     if msg.type == 'note_on':
-        print(msg.note)
+        notes.append(str(msg.note))
+print(', '.join(notes))
--- dataset/scripts/make_mono.py
+++ dataset/scripts/make_mono.py
@@ -10,7 +10,7 @@ def program_of_track(track):
     for msg in track:
         if msg.type == 'program_change':
             return msg.program
-    return 1024
+    return -1
 
 
 def time_of_track(track):
@@ -35,6 +35,24 @@ def min_note_of_track(track):
             notes.append(msg.note)
     return sum(notes)/len(notes)
 
+
+def range_of_track(track):
+    notes = []
+    for msg in track:
+        if msg.type in ('note_on', 'note_off'):
+            notes.append(msg.note)
+    if len(notes) == 0:
+        return 0
+    return max(notes)-min(notes)
+
+
+def bass_penalty(track):
+    program = program_of_track(track)
+    bass_programs = [-1, 6, 7, 77, 52, 58] + list(range(81, 128))
+    if program in bass_programs:
+        return 0
+    return 1
+
 mid = MidiFile(sys.argv[1])
 
 new_mid = MidiFile()
@@ -42,18 +60,26 @@ new_mid = MidiFile()
 tracks_metadata = []
 for t in mid.tracks:
     tracks_metadata.append((program_of_track(t), len(t),
-                            max_velocity_of_track(t), min_note_of_track(t)))
+                            max_velocity_of_track(t), min_note_of_track(t),
+                            range_of_track(t), bass_penalty(t)))
 
 t = np.array(tracks_metadata).astype('float32')
-print(t)
+# print(t)
 # t[:, 0] = np.exp(t[:, 0] / t[:, 0].max())
 # t = t / t.sum(axis=0)
-res = t[:, 2]  # * t[:, 3]
-print(t)
+res = t[:, 2] * t[:, 4] * t[:, 5]
+# print(t)
+# print(t[:, 0])
 print(res)
 index = np.argmax(res)
-
-print("Chose index {} for {}".format(index, sys.argv[1]))
+program = int(t[index, 0])
+if program != -1:
+    chosen_instrument = pretty_midi.program_to_instrument_name(program)
+else:
+    chosen_instrument = 'Unknown (Probabily Piano)'
+print("Chose index {} ({} - {}) for {}".format(index, program,
+                                               chosen_instrument,
+                                               sys.argv[1]))
 
 new_mid.tracks.append(mid.tracks[index])
 new_mid.ticks_per_beat = mid.ticks_per_beat
